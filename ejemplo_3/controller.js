0bf565499ca1081adf0d2548a4e066f5aa29e710
stiv15 <stivmarquez.ud@gmail.com>
2017-12-05 11:49:54 -0500

    Ejemplo 3
    	* Utlización de Metodos y variables del modulo
    	* Nuevas Directivas : ng-click , ng-repeat

--- ejemplo_3/controller.js
+++ ejemplo_3/controller.js
@@ -1,4 +1,24 @@
 angular.module('LaMiaPrimaApp', [])
 	.controller('PrimaController', function($scope){	
 		$scope.nombre = "Stiv";
+		$scope.nuevoEstado = {};
+		$scope.estados =[
+			{
+				estado: "Nuevo",
+				usuario: "vits"
+			},
+			{
+				estado: "En Proceso",
+				usuario: "zeuq"
+			},
+			{
+				estado: "Resuelto",
+				usuario: "sabish"
+			}
+
+		];
+		$scope.agregarEstado = function() {
+			$scope.estados.push($scope.nuevoEstado);
+			$scope.nuevoEstado = {};
+		}
 	}); 
