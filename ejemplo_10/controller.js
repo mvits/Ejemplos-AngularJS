64f5a338f5a77877697ac722e1798dce475d45e4
stiv15 <stivmarquez.ud@gmail.com>
2017-12-11 13:25:32 -0500

    Ejemplo AngularJs                 * Ejemplo implementación de Factory

---
+++ ejemplo_10/controller.js
@@ -0,0 +1,69 @@
+angular.module('ListaNotas', ["LocalStorageModule"])
+	.factory('ServicioLista', function(localStorageService){
+		var servicioLista = {} ;
+
+		servicioLista.key = "listas";
+		
+		if(localStorageService.get(servicioLista.key)){
+			servicioLista.actividades = localStorageService.get(servicioLista.key);
+		}
+		else{
+			servicioLista.actividades = [];
+		}
+
+		servicioLista.agregar = function (nuevaActividad) {
+			servicioLista.actividades.push(nuevaActividad);
+			servicioLista.actualizarLocalStorage();
+		};
+
+		servicioLista.actualizarLocalStorage = function () {
+			 localStorageService.set(servicioLista.key,servicioLista.actividades);
+		};
+
+		servicioLista.limpiar =function () {
+			servicioLista.actividades = []; 
+			servicioLista.actualizarLocalStorage();
+		};
+
+		servicioLista.getTodo = function () {
+			return servicioLista.actividades;
+		};
+
+		servicioLista.eliminarActividad = function (item) {
+			servicioLista.actividades = servicioLista.actividades.filter(function (actividad) {
+				return actividad !== item;
+			});
+		/*
+		[{},{},...]  -> servicioLista.actividades
+		Filter valida true o false 
+			 si la validación da false elimina el item del arreglo
+			 si la validación da true conserva el item del arreglo
+		*/
+
+		servicioLista.actualizarLocalStorage();
+
+		return servicioLista.getTodo();
+		};
+
+		return servicioLista;
+	})
+	.controller('ControllerLista', function($scope,ServicioLista){	
+		
+
+		$scope.todo = ServicioLista.getTodo();
+	
+		$scope.agregarNota = function() {
+			ServicioLista.agregar($scope.newActv);
+			$scope.newActv = {};
+		}
+
+		$scope.eliminarNota = function(item){
+			$scope.todo = ServicioLista.eliminarActividad(item);
+		}
+
+		$scope.limpiarNota = function(item){
+			ServicioLista.limpiar();
+		}
+
+
+	}); 
